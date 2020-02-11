b66f4503bd741dfd0dc22a202e5e2ee8ab36bc84
andremarinho <andre.bmarinho@hotmail.com>
2020-02-11 06:33:47 -0300

    Commit Inicial

---
+++ src/main/java/com/openos/api/OpenOsApiApplication.java
@@ -0,0 +1,13 @@
+package com.openos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenOsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenOsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openos/api/OpenOsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenOsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
